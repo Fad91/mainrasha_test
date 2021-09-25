a983b4df135b1ee23f9a3c126ad92f961b5f497d
Антон Куклин <homyak91@inbox.ru>
2021-09-26 00:32:02 +0400

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,5 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// custom-select
 
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+// #select - селектор для выбора элемента, который необходимо инициализировать как CustomSelect
+const select1 = new CustomSelect('#select-1');
+const select2 = new CustomSelect('#select-2');
